4ba2e75281a9e6690ca594f6a7286478e0529501
wei wang <wei-wang@Wei-MacBook-Pro-15.local>
2018-04-17 14:30:05 -0500

    more changes

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,38 +1,18 @@
 package com.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import com.boot.controller.HomeController;
+import static org.junit.Assert.assertEquals;
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+	@Test
     public void testApp()
     {
-        assertTrue( true );
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result,"my first spring boot app.");
     }
 }
